1fbfdcc440125d3af4e08ac0c0f546b05e1d1475
madekot <mgolubcov@gmail.com>
2021-07-10 15:10:49 +0300

    начало

---
+++ source/js/main.js
@@ -0,0 +1,14 @@
+import {ieFix} from './utils/ie-fix';
+import {iosVhFix} from './utils/ios-vh-fix';
+
+import {initModals} from './modules/init-modals';
+// Utils
+// ---------------------------------
+
+ieFix();
+iosVhFix();
+
+// Modules
+// ---------------------------------
+
+initModals();
