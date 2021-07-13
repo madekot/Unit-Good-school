2b77f6f12cb018ee4cf822a0c4e31064ed954886
Михаил Голубцов <mgolubcov@gmail.com>
2021-07-13 18:13:09 +0300

    js для аккардиона

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -1,7 +1,7 @@
 const accordion = () => {
-  const accordion = document.querySelector('.js-accordion');
-  const button = accordion.querySelector('.js-accordion__button');
-  const content = accordion.querySelector('.js-accordion__content');
+  // const accordion = document.querySelector('.js-accordion');
+  // const button = accordion.querySelector('.js-accordion__button');
+  // const content = accordion.querySelector('.js-accordion__content');
 };
 
 export {accordion};
